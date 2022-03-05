d15c2872dea1c874ce0618a568940dea9c3d244b
Zoli <gallikzoltan@gmail.com>
2022-03-05 20:14:59 +0100

    Redo resources, agents

--- uml_gen/uml_generator/Program.cs
+++ uml_gen/uml_generator/Program.cs
@@ -3,111 +3,118 @@
 
 namespace CatchThemAll
 {
-    public class Program
-    {
-        public static void main()
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
 
-    interface iInvItem { string Type { get; set; }
-        string GetType() { return Type; }
+    public abstract class InvItem { 
         
+
+
+
     }
-    interface iSteppable { 
+    public interface iSteppable { 
         void Step(double curTime) { return; } 
     }
-    class Virologist : iSteppable {
-        int NukiCount; int AmiCount;
-
-        List<iInvItem> items = new List<iInvItem>();
-        List<DeployedVaccine> vaccines = new List<DeployedVaccine>();
-        List<DeployedVirus> viruses = new List<DeployedVirus>();
+    public class Virologist : iSteppable {
+        List<InvItem> items = new List<InvItem>();
+        List<GeneticCode> learntCodes = new List<GeneticCode>(); 
 
         //Amin áll
         Field mezo;
-        /*iSteppable iSteppable
+        private Resource resource;
+        private System.Collections.Generic.List<Equipment> equipments;
+
+        void MoveTo(Field targetField) {  }
+
+        public void AddResource(Resource resource)
         {
-            get => default;
-            set
-            {
-            }
+            throw new System.NotImplementedException();
         }
-
-        Map Map
-        {
-            get => default;
-            set
-            {
-            }
-        }*/
-
-        bool hasItem(string item) { return false; }
-        bool canMoveTo(Field targetField) { return false; }
     }
-    class Map { List<Field> fields = new List<Field>(); }
-    class Field { }
-    class FieldLab : Field { }
-    class FieldBunker : Field { int NukiCount; int AmiCount; }
-    class FieldWarehouse : Field { }
-    abstract class Equipment : iInvItem
+    public class Map { List<Field> fields = new List<Field>();
+
+        public void GenerateMap()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class Field 
+    {
+        List<Virologist> virologists;
+        public virtual bool AcceptViro(Virologist v) { return false; }
+        public virtual bool RemoveViro(Virologist v) { return false; }
+    }
+    public class FieldLab : Field
+    {
+        private GeneticCode code; public override bool AcceptViro(Virologist v) { return true; }
+    }
+    public class FieldBunker : Field { private Resource resources; public override bool AcceptViro(Virologist v) { return true; } }
+    public class FieldWarehouse : Field
+    {
+        private Equipment equipment; public override bool AcceptViro(Virologist v) { return true; }
+    }
+    public abstract class Equipment : InvItem
+    {
+    }
+    public class EquipmentSack : Equipment
+    {
+        public int GetMaxResource()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class EquipmentGloves : Equipment
+    {
+        public bool CanAgentBeApplied()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class EquipmentCoat : Equipment
+    {
+        public bool CanAgentBeApplied()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class GeneticCode
     {
         public string Type { get; set; }
 
-        internal iInvItem iInvItem
+        public Agent agent
         {
             get => default;
             set
             {
             }
         }
+
+        public Agent CreateVirus()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Agent CreateVaccine()
+        {
+            throw new System.NotImplementedException();
+        }
     }
-    class EquipmentSack : Equipment { }
-    class EquipmentGloves : Equipment { }
-    class EquipmentCoat : Equipment { }
-    class GeneticCode : iInvItem
+    public abstract class Agent : InvItem, iSteppable
     {
-        public string Type { get; set; }
-
-        internal iInvItem iInvItem
+        public void Apply(Virologist target)
         {
-            get => default;
-            set
-            {
-            }
+            throw new System.NotImplementedException();
         }
     }
-    abstract class Agent : iInvItem
-    {
-        public string Type { get; set; }
-
-        internal iInvItem iInvItem
-        {
-            get => default;
-            set
-            {
-            }
-        }
-
-        internal DeployedAgent DeployedAgent
-        {
-            get => default;
-            set
-            {
-            }
-        }
-    }
-    class AgentVirus : Agent { }
-    class AgentVaccine : Agent { }
-    abstract class DeployedAgent { }
-    class DeployedVaccine : DeployedAgent { }
-    class DeployedVirus : DeployedAgent { }
 
     //manager classes
 
-    class Timer { List<iSteppable> steppable_reg = new List<iSteppable>(); }
-    class GameManager { Map gameMap = new Map();
+    public class Timer { List<iSteppable> steppable_reg = new List<iSteppable>();
+
+        public void Step()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class GameManager { Map gameMap = new Map();
 
         internal Map Map
         {
@@ -118,5 +125,72 @@ namespace CatchThemAll
         }
 
         void startGame() { } 
+    }
+
+    public class Resource
+    {
+        private int ami;
+        private int nuki;
+
+        public void AddAmi(int ami)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveAmi(int ami)
+        {
+            throw new System.NotImplementedException();
+        }
+        public void AddNuki(int nuki)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveNuki(int nuki)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Dance : Agent
+    {
+        public bool Move(Virologist v)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Forget : Agent
+    {
+        public bool CanRemember()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Paralyze : Agent
+    {
+        public bool Move(Virologist v)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CanCastSpell()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CanAttack()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Protect : Agent
+    {
+        public bool CanBeInfected()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
