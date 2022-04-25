5bc378be3c9572efe160c88ca3154336d0605b19
Zoli <gallikzoltan@gmail.com>
2022-04-26 01:31:15 +0200

    some shit

--- VeryGoodViroGame/src/VeryGoodViroGame/Main.java
+++ VeryGoodViroGame/src/VeryGoodViroGame/Main.java
@@ -87,10 +87,14 @@ public class Main
                             ".tin\n" + "New_Hope.tin\n" + "ParalyzedMoveStrategy_not_moves_viro.tin\n" +
                             "Return_of_the_Axe.tin\n" + "Revenge_of_the_Viro.tin\n" + "SimpleMoveStrategy_moves_viro" + ".tin\n" + "Spread_Bear_virus.tin\n" + "testconsole.tin\n" + "teszt.tin\n" + "Timer_steps_tick-based_elements.tin\n" + "User_Acceptance_Test.tin\n" + "User_Acceptance_Test_variant.tin\n" + "User_Acceptance_Test_variant2.tin\n" + "VeryBigTest.tin\n" + "Viros_move_around_craft_and_do_stuff_that_is_not_politically_correct.tin\n" + "Viro_checks_if_they_are_paralyzed.tin\n" + "Viro_interacts_with_FieldBunker.tin\n" + "Viro_interacts_with_FieldLab.tin\n" + "Viro_interacts_with_FieldWarehouse.tin\n" + "Viro_tries_to_craft_virus_and_has_enough_resource.tin\n" + "Viro_tries_to_craft_virus_but_has_not_enough_resources.tin\n" + "Viro_tries_to_put_a_virus_on_a_Viro_and_succeeds.tin\n" + "Viro_tries_to_put_a_virus_on_a_Viro_but_paralyzed.tin\n" + "Viro_tries_to_put_a_virus_on_a_Viro_but_the_other_has_a_coat.tin\n" + "Viro_tries_to_steals_equipment_and_the_other_viro_is_paralyzed.tin\n" + "Viro_tries_to_steals_resource_and_the_other_Viro_is_paralyzed.tin\n" + "Viro_tries_to_steal_equipment_but_the_other_Viro_is_not_paralyzed.tin\n" + "Viro_tries_to_steal_resource_but_the_other_Viro_is_not_paralyzed.tin\n" + "Viro_vaccinates_a_Viro.tin\n" + "Viro’s_Menace.tin\n" + "VitusDanceMoveStrategy_randomly_moves_viro.tin";
                     String[] files = inputs.split("\n");
+                    PrintStream console = System.out;
                     for(int i = 0; i < files.length; i++)
                     {
+                        PrintStream o = new PrintStream(new FileOutputStream("tout\\" + files[i]));
                         System.out.println(files[i]);
+                        System.setOut(o);
                         ConsoleIO.RunCMD("load", new String[]{files[i]});
+                        System.setOut(console);
                         ConsoleIO.RunCMD("clear", "".split(""));
                         System.out.println("\n");
                     }
