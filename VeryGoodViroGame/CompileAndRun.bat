6431e4e5a806751f5f5db88d702d71e63df294d2
Zoli <gallikzoltan@gmail.com>
2022-04-27 01:17:33 +0200

    bat files and out

---
+++ VeryGoodViroGame/CompileAndRun.bat
@@ -0,0 +1,3 @@
+javac -encoding utf-8 -d out -sourcepath src -cp out src\VeryGoodViroGame\Main.java
+cd out
+java VeryGoodViroGame.Main
--- VeryGoodViroGame/run.bat
+++ VeryGoodViroGame/run.bat
@@ -1,4 +1,1 @@
-cd src\VeryGoodViroGame\
-javac -encoding utf-8 -d ..\..\out *.java
-cd ..\..\out
 java VeryGoodViroGame.Main
